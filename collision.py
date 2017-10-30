69e56c718b433256983638270bb19470134c9ac1
group4game <33117675+group4game@users.noreply.github.com>
2017-10-30 21:42:30 +0530

    python code for task2

---
+++ collision.py
@@ -0,0 +1,20 @@
+import bge
+
+
+
+def main():
+ 
+     	cont = bge.login.getCurrentController()
+	own = cont.owner
+
+	collision = cont.sensors["collision"]
+
+
+	if collision.positive:
+		own.color = [1,0,1,True]
+	else:
+		own.color = [1,0,0,True]
+
+
+
+main()
